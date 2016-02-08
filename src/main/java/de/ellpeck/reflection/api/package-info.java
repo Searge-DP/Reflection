57a6d099052fdc1f71bd295b3c022f0e3cf622dd
Ellpeck <megamaximal@gmail.com>
2016-02-08 23:01:20 +0100

    Copyright \o/

--- src/main/java/de/ellpeck/reflection/api/package-info.java
+++ src/main/java/de/ellpeck/reflection/api/package-info.java
@@ -1,3 +1,13 @@
+/*
+ * This file ("package-info.java") is part of the Reflection Mod for Minecraft.
+ * It is created and owned by Ellpeck and distributed
+ * under the Reflection License to be found at
+ * https://github.com/Ellpeck/Reflection/blob/master/LICENSE.md
+ * View the source code at https://github.com/Ellpeck/Reflection
+ *
+ * © 2016 Ellpeck
+ */
+
 @API(owner = ReflectionAPI.MOD_NAME, provides = ReflectionAPI.API_NAME, apiVersion = ReflectionAPI.VERSION)
 package de.ellpeck.reflection.api;
 
--- src/main/java/de/ellpeck/reflection/mod/network/LightNetwork.java
+++ src/main/java/de/ellpeck/reflection/mod/network/LightNetwork.java
@@ -1,3 +1,13 @@
+/*
+ * This file ("LightNetwork.java") is part of the Reflection Mod for Minecraft.
+ * It is created and owned by Ellpeck and distributed
+ * under the Reflection License to be found at
+ * https://github.com/Ellpeck/Reflection/blob/master/LICENSE.md
+ * View the source code at https://github.com/Ellpeck/Reflection
+ *
+ * © 2016 Ellpeck
+ */
+
 package de.ellpeck.reflection.mod.network;
 
 import de.ellpeck.reflection.api.internal.IConnectionPair;
