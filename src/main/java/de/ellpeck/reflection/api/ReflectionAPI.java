905aadbb4f1b6037e86d459dc0e0bff27b06bf90
Ellpeck <megamaximal@gmail.com>
2016-02-08 22:48:54 +0100

    Bump API Version. As if anybody had used it before anyways.

--- src/main/java/de/ellpeck/reflection/api/ReflectionAPI.java
+++ src/main/java/de/ellpeck/reflection/api/ReflectionAPI.java
@@ -4,7 +4,7 @@ import de.ellpeck.reflection.api.internal.ILightNetworkHandler;
 
 public class ReflectionAPI{
 
-    public static final String VERSION = "1";
+    public static final String VERSION = "2";
 
     public static final String MOD_NAME = "Reflection";
     public static final String API_NAME = MOD_NAME+"API";
